6fc386b5607d227b7d5acb48a26377dcd07894d8
Michal Kubiče <kubicemichal@gmail.com>
2025-10-13 11:34:06 +0200

    feat: auth routes

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express, { NextFunction } from "express";
 
 const router = express.Router();
 
-router.post("/api/users/signout", (req, res) => {
-  res.send("Hi there!");
+router.post("/api/users/signout", (req, res, next: NextFunction) => {
+  try {
+    req.session = null;
+
+    res.send({});
+  } catch (e) {
+    next(e);
+  }
 });
 
 export { router as signoutRouter };
